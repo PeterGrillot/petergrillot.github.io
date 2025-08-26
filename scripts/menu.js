2b7227936a3e40a3f617efc48efde3f57b0cb48d
petergrillot <petergrillot@gmail.com>
2025-08-26 13:31:11 -0400

    update styles

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,18 @@
-(function main () {
+(function main() {
   var menu = document.querySelector(".nav");
   var menuToggle = document.querySelector(".menu-button");
   menuToggle.addEventListener("click", function () {
     menu.classList.toggle("open");
     menuToggle.classList.toggle("morphed");
-    menuToggle.innerHTML = menu.classList.contains("open") ? "&times;" : "&#9776;";
+    menuToggle.innerHTML = menu.classList.contains("open")
+      ? "&times;"
+      : "&#9776;";
+  });
+  var header = document.querySelector(".header");
+  window.addEventListener("scroll", function (event) {
+    var scroll = this.scrollY * 0.5;
+    var transform = "translateY(" + Math.round(scroll) + "px)";
+    header.style.transform = transform;
+    header.style.webkitTransform = transform;
   });
 })();
